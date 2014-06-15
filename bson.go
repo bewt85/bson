66f699c6b79ca7823efa98d0ac2c2b9a1f9f43e7
Dave Cheney <dave@cheney.net>
2014-06-15 14:54:30 +1000

    Initial import

---
+++ bson.go
@@ -0,0 +1,71 @@
+// Package bson implements encoding and decoding of BSON objects as defined
+// at http://bsonspec.org/spec.html. The mapping between BSON objects and Go
+// values is described in the documentation for the Marshal and Unmarshal functions.
+package bson
+
+import (
+	"io"
+)
+
+// Marshal returns the BSON encoding of v.
+//
+// Struct values encode as JSON objects. Each exported struct field becomes
+// a member of the object unless
+//  - the field's tag is "-", or
+//  - the field is empty and its tag specifies the "omitempty" option.
+//
+// Map values encode as JSON objects. The map's key type must be string;
+// the object keys are used directly as map keys.
+func Marshal(v interface{}) ([]byte, error) {
+	return nil, nil
+}
+
+// Unmarshal parses the BSON-encoded data and stores the result in the
+// value pointed to by v.
+//
+// Unmarshal uses the inverse of the encodings that Marshal uses,
+// allocating maps, slices, and pointers as necessary.
+//
+// Portions of data may be retained by the decoded result in v. Data should
+// not be reused.
+func Unmarshal(data []byte, v interface{}) error {
+	return nil
+}
+
+// A Decoder reads and decodes BSON objects from an input stream.
+type Decoder struct {
+	r io.Reader
+}
+
+// NewDecoder returns a new decoder that reads from r.
+func NewDecoder(r io.Reader) *Decoder {
+	return &Decoder{r: r}
+}
+
+// Decode reads the next BSON-encoded value from its input and stores it in
+// the value pointed to by v.
+//
+// See the documentation for Unmarshal for details about the conversion of
+// BSON into a Go value.
+func (d *Decoder) Decode(v interface{}) error {
+	return nil
+}
+
+// An Encoder writes BSON objects to an output stream.
+type Encoder struct {
+	w io.Writer
+}
+
+// NewEncoder returns a new encoder that writes to w.
+func NewEncoder(w io.Writer) *Encoder {
+	return &Encoder{w: w}
+}
+
+// Encode writes the BSON encoding of v to the stream, followed by a
+// newline character.
+//
+// See the documentation for Marshal for details about the conversion of Go
+// values to BSON.
+func (e *Encoder) Encode(v interface{}) error {
+	return nil
+}
---
+++ bson_test.go
@@ -0,0 +1,118 @@
+package bson
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+)
+
+var marshalTests = []struct {
+	v        interface{}
+	expected []byte
+	err      error
+}{}
+
+func TestMarshal(t *testing.T) {
+	for _, tt := range marshalTests {
+		got, err := Marshal(tt.v)
+		if err != tt.err {
+			t.Errorf("Marshal(%#v): expected err: %v, got %v", tt.v, tt.err, err)
+			continue
+		}
+		if !reflect.DeepEqual(tt.expected, got) {
+			t.Errorf("Marshal(%#v): expected: %v, got: %v", tt.v, tt.expected, got)
+		}
+	}
+}
+
+var unmarshalTests = []struct {
+	data     []byte
+	expected interface{}
+	err      error
+}{}
+
+func TestUnmarshal(t *testing.T) {
+	for _, tt := range unmarshalTests {
+		var v interface{}
+		err := Unmarshal(tt.data, &v)
+		if err != tt.err {
+			t.Errorf("Unmarshal(%v): expected err: %v, got %v", tt.data, tt.err, err)
+			continue
+		}
+		if !reflect.DeepEqual(tt.expected, v) {
+			t.Errorf("Unmarshal(%v): expected %v, got %v", tt.data, tt.expected, v)
+		}
+	}
+}
+
+func TestNewDecoder(t *testing.T) {
+	var r bytes.Buffer
+	var d interface{}
+	d = NewDecoder(&r)
+	switch d.(type) {
+	case *Decoder:
+	default:
+		t.Fatal("NewDecoder extended %T, got %T", new(Decoder), d)
+	}
+	if d == nil {
+		t.Fatal("NewDecoder returned nil *Decoder")
+	}
+}
+
+func TestNewEncoder(t *testing.T) {
+	var w bytes.Buffer
+	var e interface{}
+	e = NewEncoder(&w)
+	switch e.(type) {
+	case *Encoder:
+	default:
+		t.Fatal("NewEncoder extended %T, got %T", new(Encoder), e)
+	}
+	if e == nil {
+		t.Fatal("NewEncoder returned nil *Encoder")
+	}
+}
+
+var encoderEncodeTests = []struct {
+	v        interface{}
+	expected []byte
+	err      error
+}{}
+
+func TestEncoderEncode(t *testing.T) {
+	for _, tt := range encoderEncodeTests {
+		var w bytes.Buffer
+		e := NewEncoder(&w)
+		err := e.Encode(tt.v)
+		if err != tt.err {
+			t.Errorf("Encoder.Encode(%#v): expected err: %v, got %v", tt.v, tt.err, err)
+			continue
+		}
+		got := w.Bytes()
+		if !reflect.DeepEqual(tt.expected, got) {
+			t.Errorf("Encoder.Encode(%#v): expected: %v, got: %v", tt.v, tt.expected, got)
+		}
+	}
+}
+
+var decoderDecodeTests = []struct {
+	data     []byte
+	expected interface{}
+	err      error
+}{}
+
+func TestDecoderDecode(t *testing.T) {
+	for _, tt := range decoderDecodeTests {
+		r := bytes.NewReader(tt.data)
+		d := NewDecoder(r)
+		var v interface{}
+		err := d.Decode(&v)
+		if err != tt.err {
+			t.Errorf("Decoder.Decode(%v): expected err: %v, got %v", tt.data, tt.err, err)
+			continue
+		}
+		if !reflect.DeepEqual(tt.expected, v) {
+			t.Errorf("Decoder.Decode(%v): expected %v, got %v", tt.data, tt.expected, v)
+		}
+	}
+}
